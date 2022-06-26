0d094b228ff71988719f306a2b0c581ba704583f
Josenene8 <100051827+Josenene8@users.noreply.github.com>
2022-06-26 11:53:28 -0300

    Update MP4Handler.hx

--- source/MP4Handler.hx
+++ source/MP4Handler.hx
@@ -5,7 +5,7 @@ import flixel.FlxG;
 import flixel.FlxState;
 #if android
 import extension.videoview.VideoView;
-import android.AndroidTools;
+import extension.AndroidTools;
 #elseif windows
 import vlc.VlcBitmap;
 import openfl.events.Event;
