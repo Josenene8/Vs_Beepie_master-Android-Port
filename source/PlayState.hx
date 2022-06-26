15a275351abdc4e448821135cd39218cb09ceeda
Josenene8 <100051827+Josenene8@users.noreply.github.com>
2022-06-26 13:44:39 -0300

    Update PlayState.hx

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -2822,7 +2822,10 @@ class PlayState extends MusicBeatState
 	}
 					switch(SONG.song.toLowerCase())
 					{
-						playCutscene(SUtil.getPath() + "assets/videos/Cutscene1.mp4");
+						case 'stalker':
+						PlayState.SONG = Song.loadFromJson(poop, 'stalker');
+						playCutscene('Cutscene1');
+						playCutscene('Cutscene1');	
 						default:
 							FlxTransitionableState.skipNextTransIn = true;
 							FlxTransitionableState.skipNextTransOut = true;
